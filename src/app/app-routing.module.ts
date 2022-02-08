02bd89a2f3d857b58c50f920e70022ce0bb96cb7
Roberto Guerra <robequiron@gmail.com>
2022-02-08 14:35:48 +0000

    Inicio game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
+import { ScoreComponent } from './pages/score/score.component';
+/**
+ * Rutas
+ */
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path:'', component:HomeComponent},
+  {path:'play', component: PlayComponent},
+  {path:'score', component:ScoreComponent},
+  {path:'**', component:HomeComponent},  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
+import { ScoreComponent } from './pages/score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
